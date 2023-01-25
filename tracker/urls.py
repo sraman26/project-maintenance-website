7b3d6e6958cd75569d109c9250eb666cc14eb27e
Sitara Raman <sitara.r.raman@gmail.com>
2023-01-24 20:14:27 -0600

    feature 7 done

--- tracker/urls.py
+++ tracker/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("", redirect_to_home, name="home"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
