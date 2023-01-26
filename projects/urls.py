8e9bb31c7409d3c3a53627f149d59299734ed5d0
Sitara Raman <sitara.r.raman@gmail.com>
2023-01-25 18:35:58 -0600

    feature 13 and 14 done

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from projects.views import (
-    list_projects,
-)
+from .views import list_projects, create_project, show_project
 
 urlpatterns = [
     path("", list_projects, name="list_projects"),
+    path("create/", create_project, name="create_project"),
+    path("<int:id>/", show_project, name="show_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
 from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm
+
 
 # Create your views here.
 @login_required
@@ -8,3 +10,27 @@ def list_projects(request):
     project_list = Project.objects.filter(owner=request.user)
     context = {"list_projects": project_list}
     return render(request, "projects/project_list.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/project_create.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = Project.objects.get(id=id)
+    context = {"project": project}
+    return render(request, "projects/project_detail.html", context)
